d39205b59b4a8f7fa7729b9ff7564a2d8073e56f
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-01-19 00:28:49 +0300

    add

---
+++ webservice/src/main/java/com/example/webservice/controller/PersonneController.java
@@ -0,0 +1,27 @@
+package com.example.webservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.webservice.exception.ResourceNotFoundException;
+import com.example.webservice.model.Personne;
+import com.example.webservice.repository.PersonneRepository;
+
+@RestController
+@RequestMapping("/")
+public class PersonneController {
+	@Autowired
+    private PersonneRepository personneRepository;
+	
+	// Utilisateur par id
+	@GetMapping("/personne/{id}")
+	public ResponseEntity<Personne> getPersonneById(@PathVariable(value = "id") long id) throws Exception
+	{
+		Personne personne = personneRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Personne non trouvé" ));
+        return ResponseEntity.ok().body(personne);
+	}
+}
---
+++ webservice/src/main/java/com/example/webservice/controller/SignalementController.java
@@ -0,0 +1,144 @@
+package com.example.webservice.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.PutMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.webservice.exception.ResourceNotFoundException;
+import com.example.webservice.model.Signalement;
+import com.example.webservice.repository.SignalementRepository;
+
+@RestController
+@RequestMapping("/")
+public class SignalementController {
+	@Autowired
+    private SignalementRepository signalementRepository;
+
+	//Recherche avancée
+	@GetMapping("/rechercherSignalement/{idRegion}/{idType}/{status}")
+	public List<Signalement> rechercherSignalement(@PathVariable(value="idRegion") Integer idRegion,@PathVariable(value="idType") Long idType,@PathVariable(value="status") String status) throws Exception
+	{
+		return this.signalementRepository.rechercherSignalement(idRegion,idType,status);
+	}
+
+	
+	//Notification
+	@GetMapping("/listNotification")
+	public  List<List<Object>> getListNotification() throws Exception{
+		return this.signalementRepository.getListNotification();
+	}
+
+	//Liste des nouveaux signalements non affectés
+	@GetMapping("/listNewSignalement")
+	public  List<List<Object>> getListNewSignalement() throws Exception{
+		return this.signalementRepository.findSignalementNotAffected();
+	}
+
+	//Liste des signalements affectés
+	@GetMapping("/listAffectedSignalement")
+	public  List<List<Object>> getListAffectedSignalement() throws Exception{
+		return this.signalementRepository.findAffectedSignalement();
+	}
+	
+	//Affecter un signalement
+	@PutMapping("/affecterSignalement/{idSignalement}/{idRegion}")
+	public void affecterSignalement(@PathVariable(value="idSignalement") Long idSignalement,@PathVariable(value="idRegion") Integer idRegion){
+		Signalement signalement = signalementRepository.findById(idSignalement).get();
+		signalement.setIdRegion(idRegion);
+		signalementRepository.save(signalement);
+	} 
+
+	//Terminer un signalement
+	@PutMapping("/terminerSignalement/{id}")
+	public void terminerSignalement(@PathVariable(value="id") Long signalementId){
+		Signalement signalement = signalementRepository.findById(signalementId).get();
+		signalement.setStatus("termine");
+		signalementRepository.save(signalement);
+	}
+
+	//Supprimer un signalement
+	@DeleteMapping("/supprimerSignalement/{id}")
+	public void deleteSignalement(@PathVariable(value="id") Long signalementId){
+		signalementRepository.deleteById(signalementId);
+	}
+
+	// Signalements dans une région
+	@GetMapping("/signalements/idRegion")
+	public List<Signalement> getSignalementByIdRegion(@RequestParam long idRegion) throws Exception
+	{
+		return this.signalementRepository.findByIdRegion(idRegion);
+	}
+	//One
+	
+
+	@GetMapping("/signalement/{id}")
+    public List<List<Object>> getSignalementById(@PathVariable(value = "id") Long sId) throws ResourceNotFoundException {
+		return this.signalementRepository.findOneSignalement(sId);
+	}
+    
+
+	// all
+	@GetMapping("/signalements")
+	public List<Signalement> getAllSignalement() throws Exception
+	{
+		return this.signalementRepository.findAll();
+	}
+
+	// get signalement by id
+	@GetMapping("/signalementRegion/{id}")
+	public ResponseEntity<Signalement> getRegionById(@PathVariable(value = "id") long idSignalement) throws Exception
+	{
+		Signalement signalement = signalementRepository.findById(idSignalement).orElseThrow(()->new ResourceNotFoundException("Signalement not found for this id :: " ));
+        return ResponseEntity.ok().body(signalement);
+	}
+	
+	// insert
+	@PostMapping("/signalement")
+	public Signalement createSignalement(@RequestBody Signalement signalement) throws Exception
+	{
+		return this.signalementRepository.save(signalement);
+	}
+	
+	// update
+	@PutMapping("/signalement/{id}")
+	public ResponseEntity<Signalement> updateSignalement(@PathVariable(value = "id") Long idSignalement , @Valid @RequestBody Signalement signalementDetails) throws Exception
+	{
+		Signalement signalement = signalementRepository.findById(idSignalement).orElseThrow(()->new ResourceNotFoundException("Signalement not found for this id"));
+		signalement.setIdRegion(signalementDetails.getIdRegion());
+		signalement.setIdType(signalementDetails.getIdType());
+		signalement.setStatus(signalementDetails.getStatus());
+        return ResponseEntity.ok(this.signalementRepository.save(signalement));
+	}
+	
+	// delete
+	@DeleteMapping("/signalement/{id}")
+	public Map<String,Boolean> deleteSignalement(@PathVariable(value = "id") long idSignalement) throws Exception
+	{
+		Signalement signalement = signalementRepository.findById(idSignalement).orElseThrow(()->new ResourceNotFoundException("Signalement not found for this id"));
+        this.signalementRepository.delete(signalement);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted",Boolean.TRUE);
+        return response;
+	}
+}
