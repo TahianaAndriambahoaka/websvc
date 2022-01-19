2d75a821fd4ac420efba3acd986d0dbd282985ae
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-01-20 00:25:09 +0300

    fix

--- Java Webservice/webservice/src/main/java/com/example/webservice/controller/SignalementController.java
+++ Java Webservice/webservice/src/main/java/com/example/webservice/controller/SignalementController.java
@@ -7,9 +7,9 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,155 +31,175 @@ import com.example.webservice.repository.SignalementRepository;
 @RequestMapping("/")
 public class SignalementController {
 	@Autowired
-    private SignalementRepository signalementRepository;
+	private SignalementRepository signalementRepository;
 
-	//Recherche avancée
+	// Recherche avancée
 	@GetMapping("/rechercherSignalement/{idRegion}/{idType}/{status}")
-	public List<Signalement> rechercherSignalement(@PathVariable(value="idRegion") Integer idRegion,@PathVariable(value="idType") Long idType,@PathVariable(value="status") String status) throws Exception
-	{
-		return this.signalementRepository.rechercherSignalement(idRegion,idType,status);
+	public List<Signalement> rechercherSignalement(@PathVariable(value = "idRegion") Integer idRegion,
+			@PathVariable(value = "idType") Long idType, @PathVariable(value = "status") String status)
+			throws Exception {
+		return this.signalementRepository.rechercherSignalement(idRegion, idType, status);
 	}
 
+<<<<<<< Updated upstream
 	//Notification
+=======
+	// Notification
+>>>>>>> Stashed changes
 	@GetMapping("/listNotification")
-	public  List<List<Object>> getListNotification() throws Exception{
+	public List<List<Object>> getListNotification() throws Exception {
 		return this.signalementRepository.getListNotification();
 	}
 
-	//Liste des nouveaux signalements non affectés
+	// Liste des nouveaux signalements non affectés
 	@GetMapping("/listNewSignalement")
-	public  List<List<Object>> getListNewSignalement() throws Exception{
+	public List<List<Object>> getListNewSignalement() throws Exception {
 		return this.signalementRepository.findSignalementNotAffected();
 	}
 
-	//Liste des signalements affectés
+	// Liste des signalements affectés
 	@GetMapping("/listAffectedSignalement")
+<<<<<<< Updated upstream
 	public  List<List<Object>> getListAffectedSignalement() throws Exception
 	{
+=======
+	public List<List<Object>> getListAffectedSignalement() throws Exception {
+>>>>>>> Stashed changes
 		return this.signalementRepository.findAffectedSignalement();
 	}
-	
-	//Affecter un signalement
+
+	// Affecter un signalement
 	@PutMapping("/affecterSignalement/{idSignalement}/{idRegion}")
-	public void affecterSignalement(@PathVariable(value="idSignalement") Long idSignalement,@PathVariable(value="idRegion") Integer idRegion){
+	public void affecterSignalement(@PathVariable(value = "idSignalement") Long idSignalement,
+			@PathVariable(value = "idRegion") Integer idRegion) {
 		Signalement signalement = signalementRepository.findById(idSignalement).get();
 		signalement.setIdRegion(idRegion);
 		signalementRepository.save(signalement);
-	} 
+	}
 
-	//Terminer un signalement
+	// Terminer un signalement
 	@PutMapping("/terminerSignalement/{id}")
+<<<<<<< Updated upstream
 	public void terminerSignalement(@PathVariable(value="id") Long signalementId)
 	{
+=======
+	public void terminerSignalement(@PathVariable(value = "id") Long signalementId) {
+>>>>>>> Stashed changes
 		Signalement signalement = signalementRepository.findById(signalementId).get();
 		signalement.setStatus("termine");
 		signalementRepository.save(signalement);
 	}
 
-	//Supprimer un signalement
+	// Supprimer un signalement
 	@DeleteMapping("/supprimerSignalement/{id}")
-	public void deleteSignalement(@PathVariable(value="id") Long signalementId){
+	public void deleteSignalement(@PathVariable(value = "id") Long signalementId) {
 		signalementRepository.deleteById(signalementId);
 	}
 
 	// Signalements dans une région
 	@GetMapping("/signalements/idRegion")
-	public List<Signalement> getSignalementByIdRegion(@RequestParam long idRegion) throws Exception
-	{
+	public List<Signalement> getSignalementByIdRegion(@RequestParam long idRegion) throws Exception {
 		return this.signalementRepository.findByIdRegion(idRegion);
 	}
 
-	//One
+	// One
 	@GetMapping("/signalement/{id}")
-    public List<List<Object>> getSignalementById(@PathVariable(value = "id") Long sId) throws ResourceNotFoundException {
+	public List<List<Object>> getSignalementById(@PathVariable(value = "id") Long sId)
+			throws ResourceNotFoundException {
 		return this.signalementRepository.findOneSignalement(sId);
 	}
-    
 
 	// all
 	@GetMapping("/signalements")
-	public List<Signalement> getAllSignalement() throws Exception
-	{
+	public List<Signalement> getAllSignalement() throws Exception {
 		return this.signalementRepository.findAll();
 	}
 
 	// get signalement by id
 	@GetMapping("/signalementRegion/{id}")
-	public ResponseEntity<Signalement> getRegionById(@PathVariable(value = "id") long idSignalement) throws Exception
-	{
-		Signalement signalement = signalementRepository.findById(idSignalement).orElseThrow(()->new ResourceNotFoundException("Signalement not found for this id :: " ));
-        return ResponseEntity.ok().body(signalement);
+	public ResponseEntity<Signalement> getRegionById(@PathVariable(value = "id") long idSignalement) throws Exception {
+		Signalement signalement = signalementRepository.findById(idSignalement)
+				.orElseThrow(() -> new ResourceNotFoundException("Signalement not found for this id :: "));
+		return ResponseEntity.ok().body(signalement);
 	}
-	
+
 	// insert
 	@PostMapping("/signalement")
-	public Signalement createSignalement(@RequestBody Signalement signalement) throws Exception
-	{
+	public Signalement createSignalement(@RequestBody Signalement signalement) throws Exception {
 		return this.signalementRepository.save(signalement);
 	}
-	
+
 	// update
 	@PutMapping("/signalement/{id}")
-	public ResponseEntity<Signalement> updateSignalement(@PathVariable(value = "id") Long idSignalement , @Valid @RequestBody Signalement signalementDetails) throws Exception
-	{
-		Signalement signalement = signalementRepository.findById(idSignalement).orElseThrow(()->new ResourceNotFoundException("Signalement not found for this id"));
+	public ResponseEntity<Signalement> updateSignalement(@PathVariable(value = "id") Long idSignalement,
+			@Valid @RequestBody Signalement signalementDetails) throws Exception {
+		Signalement signalement = signalementRepository.findById(idSignalement)
+				.orElseThrow(() -> new ResourceNotFoundException("Signalement not found for this id"));
 		signalement.setIdRegion(signalementDetails.getIdRegion());
 		signalement.setIdType(signalementDetails.getIdType());
 		signalement.setStatus(signalementDetails.getStatus());
-        return ResponseEntity.ok(this.signalementRepository.save(signalement));
+		return ResponseEntity.ok(this.signalementRepository.save(signalement));
 	}
-	
+
 	// delete
 	@DeleteMapping("/signalement/{id}")
-	public Map<String,Boolean> deleteSignalement(@PathVariable(value = "id") long idSignalement) throws Exception
-	{
-		Signalement signalement = signalementRepository.findById(idSignalement).orElseThrow(()->new ResourceNotFoundException("Signalement not found for this id"));
-        this.signalementRepository.delete(signalement);
-        Map<String,Boolean> response = new HashMap<>();
-        response.put("deleted",Boolean.TRUE);
-        return response;
+	public Map<String, Boolean> deleteSignalement(@PathVariable(value = "id") long idSignalement) throws Exception {
+		Signalement signalement = signalementRepository.findById(idSignalement)
+				.orElseThrow(() -> new ResourceNotFoundException("Signalement not found for this id"));
+		this.signalementRepository.delete(signalement);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
 	}
-	
-	//Recherche avancée BackOffice
+
+	// Recherche avancée BackOffice
 	@GetMapping("/rechercherSignalement/annee={annee}&moisDebut={moisDebut}&moisFin={moisFin}&idType={idType}")
-	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value="annee") Integer annee, @PathVariable(value="moisDebut") Integer moisDebut, @PathVariable(value="moisFin") Integer moisFin, @PathVariable(value="idType") Long idType) throws Exception
-	{
+	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value = "annee") Integer annee,
+			@PathVariable(value = "moisDebut") Integer moisDebut, @PathVariable(value = "moisFin") Integer moisFin,
+			@PathVariable(value = "idType") Long idType) throws Exception {
 		return this.signalementRepository.rechercherSignalementBackOffice(annee, moisDebut, moisFin, idType);
 	}
-	//Recherche avancée BackOffice
+
+	// Recherche avancée BackOffice
 	@GetMapping("/rechercherSignalement/annee={annee}/idType={idType}")
-	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value="annee") Integer annee, @PathVariable(value="idType") Long idType) throws Exception
-	{
+	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value = "annee") Integer annee,
+			@PathVariable(value = "idType") Long idType) throws Exception {
 		return this.signalementRepository.rechercherSignalementBackOffice(annee, idType);
 	}
-	//Recherche avancée BackOffice
+
+	// Recherche avancée BackOffice
 	@GetMapping("/rechercherSignalement/annee{annee}&moisDebut={moisDebut}&moisFin={moisFin}")
-	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value="annee") Integer annee, @PathVariable(value="moisDebut") Integer moisDebut, @PathVariable(value="moisFin") Integer moisFin) throws Exception
-	{
+	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value = "annee") Integer annee,
+			@PathVariable(value = "moisDebut") Integer moisDebut, @PathVariable(value = "moisFin") Integer moisFin)
+			throws Exception {
 		return this.signalementRepository.rechercherSignalementBackOffice(annee, moisDebut, moisFin);
 	}
-	//Recherche avancée BackOffice
+
+	// Recherche avancée BackOffice
 	@GetMapping("/rechercherSignalement/annee={annee}")
-	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value="annee") Integer annee) throws Exception
-	{
+	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value = "annee") Integer annee)
+			throws Exception {
 		return this.signalementRepository.rechercherSignalementBackOffice(annee);
 	}
-	//Recherche avancée BackOffice
+
+	// Recherche avancée BackOffice
 	@GetMapping("/rechercherSignalement/moisDebut={moisDebut}&moisFin={moisFin}")
-	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value="moisDebut") Integer moisDebut, @PathVariable(value="moisFin") Integer moisFin) throws Exception
-	{
+	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value = "moisDebut") Integer moisDebut,
+			@PathVariable(value = "moisFin") Integer moisFin) throws Exception {
 		return this.signalementRepository.rechercherSignalementBackOffice(moisDebut, moisFin);
 	}
-	//Recherche avancée BackOffice
+
+	// Recherche avancée BackOffice
 	@GetMapping("/rechercherSignalement/idType={idType}")
-	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value="idType") Long idType) throws Exception
-	{
+	public List<Signalement> rechercherSignalementBackOffice(@PathVariable(value = "idType") Long idType)
+			throws Exception {
 		return this.signalementRepository.rechercherSignalementBackOffice(idType);
 	}
+
 	// getNbSignalementParType BackOffice
+	// @CrossOrigin(origins = "http://localhost/")
 	@GetMapping("/rechercherSignalement/getNbSignalementParType")
-	List<Object> getNbSignalementParType()
-	{
+	List<Object> getNbSignalementParType() {
 		return this.signalementRepository.getNbSignalementParType();
 	}
 }
